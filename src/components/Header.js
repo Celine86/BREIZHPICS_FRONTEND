4c0073af00197b7e55c30aa4f0bf120f0dd20a05
Celine86 <celine.obringer@live.fr>
2022-01-11 22:33:17 +0100

    FrontEnd with React, general design

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+export default function Header () {
+    return (
+        <div className="header">
+            <h1>BREIZHPICS</h1>
+            <p>Partagez vos plus belles photos !</p>
+            <div><p>Se Connecter | S'inscrire</p></div>
+        </div>
+    )
+}
+
